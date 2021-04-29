1a865aff03e3ede6da6f67ffc972a59c61a7fd1c
Cris Stanza <crisstanza@users.noreply.github.com>
2021-04-29 15:34:04 -0300

    CSS lib path; docs.

--- js/DateDiff.js
+++ js/DateDiff.js
@@ -8,7 +8,9 @@ DateDiff.diff = function(date1Str, date2Str) {
 	return { years: 3, months: 2, days: 1, hours: 4, minutes: 5 };
 };
 
+/*
+ * @return A number between 0 (inclusive) and 11 (inclusive).
+ */
 DateDiff.diffInMonths = function(date1Str, date2Str) {
 	return DateDiff.diff(date1Str, date2Str).months;
 };
-
